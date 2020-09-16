f6749778e0e154b91de1f38d05c5a0044aac87de
Hala Alkhellow <halakhellow1996@gmail.com>
2020-09-16 23:10:17 +0300

    render feedback message

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,7 @@
 let socket = io.connect("http://localhost:4000/");
 
 let output = document.getElementById("output"),
+  feedback = document.getElementById("feedback"),
   name = document.getElementById("name"),
   message = document.getElementById("message"),
   btn = document.getElementById("send");
@@ -13,9 +14,14 @@ btn.onclick = () => {
 };
 
 socket.on("chat", (data) => {
+  feedback.innerHTML = "";
   output.innerHTML += `<p> <span>${data.name}</span> : ${data.message} </p>`;
 });
 
 message.onkeypress = () => {
   socket.emit("typing", name.value);
 };
+
+socket.on("typing", (data) => {
+  feedback.innerHTML = `<p> <em>${data} is typing ...</em> </p>`;
+});

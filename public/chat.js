0cbe4fa5d93ff402e2daa13631b49b4882008c2c
Hala Alkhellow <halakhellow1996@gmail.com>
2020-09-16 22:27:56 +0300

    connect to the server and emit message

---
+++ public/chat.js
@@ -0,0 +1,13 @@
+let socket = io.connect("http://localhost:4000/");
+
+let output = document.getElementById("output"),
+  name = document.getElementById("name"),
+  message = document.getElementById("message"),
+  btn = document.getElementById("send");
+
+btn.onclick = () => {
+  socket.emit("chat", {
+    name: name.value,
+    message: message.value,
+  });
+};
